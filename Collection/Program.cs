6d964f46ae9203ed48a72947d5ba8813f8329472
GokhanZengin07 <igokhanzengin@gmail.com>
2022-05-26 14:42:11 +0300

    Add project files.

---
+++ Collection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var salmons = new List<string>();
+            salmons.Add("asd");
+            salmons.Add("qwe");
+            salmons.Add("rty");
+            foreach (var salmon in salmons)
+            {
+                Console.WriteLine(salmon);
+            }
+
+            var salmons2 = new List<string> { "chin", "coho", "pink" };
+            for (int i = 0; i < salmons2.Count; i++)
+            {
+                Console.WriteLine(salmons2[i]);
+            }
+
+            var theGalaxies = new List<Galaxy>
+        {
+            new Galaxy() { Name="Tadpole", MegaLightYears=400},
+            new Galaxy() { Name="Pinwheel", MegaLightYears=25},
+            new Galaxy() { Name="Milky Way", MegaLightYears=0},
+            new Galaxy() { Name="Andromeda", MegaLightYears=3}
+        };
+        }
+
+        public class Galaxy
+        {
+            public string Name { get; set; }
+            public int MegaLightYears { get; set; }
+        }
+    }
+}
---
+++ Day_1/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/*
+ * The problem
+ * You are given an NxN 2D matrix that represents an image. 
+ * Rotate the image by 90 degree(cw)
+ * 
+ */
+
+/*
+ * Solution Step-1
+ * Transpose Matrix
+ * swap(array[i][j],[j][i])
+ */
+namespace Day_1
+{
+   
+    
+    public class Rotate
+    {
+        
+
+        public int[][] rotateImage2(int[][] matrix)
+        {
+            int N = matrix.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N/2; j++)
+                {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][N-1-j];
+                    matrix[j][N-1-j] = temp;
+                }
+            }
+            return matrix;
+        }
+
+        public int[][] rotateImage(int[][] matrix)
+        {
+            int N = matrix.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i; j < N; j++)
+                {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+            return matrix;
+        }
+    }
+    
+}
